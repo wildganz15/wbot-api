1c30d98a30dc68f304124723f06d869fef96f4cd
wildganz15 <128929054+wildganz15@users.noreply.github.com>
2023-03-26 11:43:21 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 
 global.creator = 'wildganz'// yourname
 global.MONGO_DB_URI = "mongodb+srv://wganz15:jigaz2165@cluster0.pd3el11.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "xxjoktqdqpptlmtv" //isi apa aja bebas
+global.ACTIVATION_TOKEN_SECRET = "gxgqvbdjxojcszvu" //isi apa aja bebas
 global.your_email = "wildofffc@gmail.com" //email
 global.email_password = "jigaz2165" //application password email
 global.limitCount = 10000
